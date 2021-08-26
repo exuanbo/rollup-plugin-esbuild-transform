c94bbc27c0922cb0ac31992ce6e3e3f4311a2fd1
exuanbo <exuanbo@protonmail.com>
2021-08-26 14:37:35 +0100

    build: use `esbuild.buildSync()`

--- scripts/build.js
+++ scripts/build.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 'use strict'
 
-const { build } = require('esbuild')
+const { buildSync } = require('esbuild')
 const pkg = require('../package.json')
 
-;(async () => {
-  await build({
-    bundle: true,
-    color: true,
-    entryPoints: ['src/index.ts'],
-    external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)],
-    format: 'esm',
-    logLevel: 'info',
-    outfile: '.cache/index.js',
-    platform: 'node',
-    write: true
-  })
-})()
+buildSync({
+  bundle: true,
+  color: true,
+  entryPoints: ['src/index.ts'],
+  external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)],
+  format: 'esm',
+  logLevel: 'info',
+  outfile: '.cache/index.js',
+  platform: 'node',
+  write: true
+})
