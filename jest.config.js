114d4e20ad722287b10cd585af9e2de6e9762e49
exuanbo <exuanbo@protonmail.com>
2022-05-13 18:10:40 +0100

    test: set `isolatedModules` to true for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/__tests__/**/*.test.ts?(x)']
